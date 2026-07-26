7b43ae7ff8757c3c19f504afccdf145d1e4044e6
Kenezu17 <jala.fumar.sjc@phinmaed.com>
2026-07-26 12:58:15 +0800

    Fix: Set hardcoded production URL fallback for safety

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     'http://localhost:5173',
-     
+    'https:// cvt-nc2msir2y-kenezu17s-projects.vercel.app'
      ],
     allow_credentials=True,
     allow_methods=['*'],
