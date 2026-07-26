13796b48ac18dedb85ae0bb02afa4ef9d694fd8f
Kenezu17 <jala.fumar.sjc@phinmaed.com>
2026-07-26 14:02:20 +0800

    new

--- ConvertX/src/services/api.js
+++ ConvertX/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const APIURL = axios.create({
-    baseURL: import.meta.env.VITE_APIURL || 'http://localhost:5173',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5173',
 });
 
 export default APIURL;
